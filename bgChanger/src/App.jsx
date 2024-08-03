1efa98e6de0097e92e830bf04347cb825cbaf1b6
mahtvakanksha <niharikagothwal8@gmail.com>
2024-08-03 18:24:29 +0530

    bgcolor done

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,25 +1,38 @@
 import { useState } from "react"
 
 function App() {
-  const [color, setColor] = useState("slate")
+  const [color, setColor] = useState("#99582a")
 
   return (
     <>
   <div className="h-screen w-screen" style={{backgroundColor:color}}>
-    <div className="fixed bottom-12 border-r-2
-    p-3">
+    <div className="fixed bottom-12
+bg-slate-300 m-1   p-3">
       <div className="bg-slate-100 flex flex-wrap flex-col gap-6 justify-center">
-        <button className="border-double p-2 bg-slate-400 text-black">{color}</button>
-        <button className="border-double p-2 bg-slate-400 text-black">{color}</button>
-        <button className="border-double p-2 bg-slate-400 text-black">{color}</button>
-        <button className="border-double p-2 bg-slate-400 text-black">{color}</button>
-
-        <button className="border-double p-2 bg-slate-400 text-black">{color}</button>
-        <button className="border-double p-2 bg-slate-400 text-black">{color}</button>
-        <button className="border-double p-2 bg-slate-400 text-black">{color}</button>
-        <button className="border-double p-2 bg-slate-400 text-black">{color}</button>
-        <button className="border-double p-2 bg-slate-400 text-black">{color}</button>
-      </div>
+        <button
+        onClick={()=>setColor("red")} className="border-double p-2 bg-slate-400 text-black " 
+        style={{backgroundColor:"red"}}>Red</button>
+        <button
+        onClick={()=>setColor("green")} className="border-double p-2 bg-slate-400 text-black " 
+        style={{backgroundColor:"green"}}>green
+        </button>   
+        <button
+        onClick={()=>setColor("yellow")} className="border-double p-2 bg-slate-400 text-black " 
+        style={{backgroundColor:"yellow"}}>yellow</button>   
+        <button
+        onClick={()=>setColor("pink")} className="border-double p-2 bg-slate-400 text-black " 
+        style={{backgroundColor:"pink"}}>pink</button>   
+        <button
+        onClick={()=>setColor("orange")} className="border-double p-2 bg-slate-400 text-black " 
+        style={{backgroundColor:"orange"}}>orange</button>   
+        <button
+        onClick={()=>setColor("white")} className="border-double p-2 bg-slate-400 text-black " 
+        style={{backgroundColor:"white"}}>white</button>   
+        <button
+        onClick={()=>setColor("black")} className="border-double p-2 bg-slate-400 text-black " 
+        style={{backgroundColor:""}}>black</button>
+        
+           </div>
     </div>
   </div>
     </>
